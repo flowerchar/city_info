1e64fe80207597099c1ca848de6720feb7481938
unknown <flowerchar2001@gmail.com>
2024-03-27 21:42:14 +0800

    feat: 增加黑龙江部分城市

--- 03-test.py
+++ 03-test.py
@@ -1,18 +1,24 @@
-# import requests
-# from lxml import etree
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-# }
+import requests
+from lxml import etree
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+}
 # #
-# url = "https://www.gov.cn/yaowen/liebiao/home_301.htm" # ?categoryNum=001001&pageIndex=100
-# response = requests.get(url, headers=headers)
-# response.encoding = response.apparent_encoding
-# html = etree.HTML(response.text)
-# # element_a_text = html.xpath('//ul[@barrier-free-idx="127"]//text()')
+
+url = "http://szb.shaoyangnews.net/syrb/html/2018-01/31/node_3.htm" # ?categoryNum=001001&pageIndex=100
+response = requests.get(url, headers=headers)
+response.encoding = response.apparent_encoding
+html = etree.HTML(response.text)
+# print(response.text)
+element_a_text = html.xpath('//table[@cellpadding="1"]//tr/td[2]/a/div/text()')
+# print(element_a_text)
+# element_a_text = list(filter(lambda x: x != '\xa0', element_a_text))
+# element_a_date = html.xpath('//table[@width="96%"]//td[@width="80"]/a/text()')
+# element_a_href = html.xpath('//table[@width="96%"]//td[@width="70"]/a/@href')
 # element_a_text = html.xpath('//li//a/text()')
 # content = ''.join(element_a_text).strip()
 # print(content)
-#
+print(element_a_text)
 # from tqdm import tqdm
 # from time import sleep
 # for i in tqdm(range(100)):
@@ -27,6 +33,8 @@
 # print("[2023-05-23] 推动科学技术更好造福各国人民——习近平主席致2023中关村论坛的贺信激励各界携手促进科技创新")
 # print("[2023-05-23] 小微企业金融服务再加力")
 
-li = ['[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2018-12-30]', '[2018-12-30]', '[2018-12-30]', '[2018-12-30]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-28]', '[2018-12-28]', '[2018-12-27]']
-li2 = list(map(lambda x:x[1:-1:], li))
-print(li2)
+# li = ['[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2019-01-01]', '[2018-12-30]', '[2018-12-30]', '[2018-12-30]', '[2018-12-30]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-29]', '[2018-12-28]', '[2018-12-28]', '[2018-12-27]']
+# li2 = list(map(lambda x:x[1:-1:], li))
+# print(li2)
+
+# resp = requests.get('http://www.donganqu.gov.cn/list.asp?class=278&page=61')
